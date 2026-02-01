fc2dd3dea668b6436b9051d0950c0347c133ba4e
KIRTI RANI <kirtirani0820@gmail.com>
2026-02-01 22:43:12 +0530

    new edit

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -8,7 +8,6 @@ function useCurrencyInfo(currency){
         fetch(`https://v6.exchangerate-api.com/v6/fe7dc5d35ff37a3ee3156203/latest/${currency}`)
         .then((res) => res.json())
         .then((res) => setData(res.conversion_rates))},[currency]);
-         console.log(data);
     return data
 }
 
